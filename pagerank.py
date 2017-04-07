9a0c75b686585c6ac42f05df48950b9f42823a2d
laijiayi <jiayilai@usc.edu>
2017-04-07 16:49:49 -0700

    Pagerank File Generator
    PyCharm python project used to generate page rank file.

---
+++ pagerank.py
@@ -0,0 +1,11 @@
+import networkx as nx
+base = "/Library/WebServer/Documents/solr-6.5.0/crawl_data/"
+target = "/Users/laijiayi/PycharmProjects/pagerank/external_pageRankFile.txt"
+G = nx.read_edgelist("/Users/laijiayi/IdeaProjects/hw4_572/src/main/resources/Edgelist.txt", create_using=nx.DiGraph())
+pr = nx.pagerank(G, alpha=0.85, personalization=None, max_iter=30, tol=1e-06, nstart=None, weight='weight',dangling=None)
+file = open(target, "w")
+for k, v in pr.items():
+    print(k, v)
+    file.write(base + "%s=%f\n"%(k, v));
+file.flush();
+file.close();
